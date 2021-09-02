437256f45cc77812ac9b3735a658e050c7a6eae7
GordonMunro <82704461+GordonMunro@users.noreply.github.com>
2021-09-02 13:30:16 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+// VARIABLES
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const btnReset = document.querySelector('.btn_reset');
+let missed = 0;
+
